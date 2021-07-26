999eea89270f11fbb07beff2d38e8fa2340a4672
“Muhammad <mmuttaki@uoguelph.ca>
2021-07-26 13:57:16 -0400

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
+// Some changes
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
