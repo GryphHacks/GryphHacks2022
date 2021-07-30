7f936e99046134fadf39e048075034471130e335
“Muhammad <mmuttaki@uoguelph.ca>
2021-07-30 10:50:19 -0400

    Nabvar reverted

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,90 +1,68 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { useHistory } from "react-router-dom";
 import { logOut } from '../actions/user';
 import firebase from '../firebase';
-import createBrowserHistory from '../history';
 
-class Navigation extends React.Component {
+const Navigation = () => {
+  const isAuthenticated = useSelector(state => state.user.isAuthenticated );
+  const dispatch = useDispatch();
+  const history = useHistory();
 
+  const logOutWithFirebase = () => {
+      firebase.auth().signOut().then(function() {
+        // log out successful
+        dispatch(logOut());
+        console.log('Log out successful.')
+        history.push('/')
+      }).catch(function(error) {
+        console.log(error)
+      }); 
+  };
 
-  logOutWithFirebase = () => {
-    firebase.auth().signOut().then(() => {
-      // log out successful
-      this.props.logOut();
-      console.log('Log out successful.')
-      createBrowserHistory.push('/')
-    }).catch(function (error) {
-      console.log(error)
-    });
-  }
-
-  renderAuthenticatedLinks() {
-    if (this.props.isAuthenticated) {
-      return (
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/faq' className="nav-link">FAQ</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/contact' className="nav-link">Contact Us</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/dashboard' className="nav-link">Dashboard</Link>
-          </li>
-          <li className="nav-item">
-            <button className='nav-link' onClick={this.logOutWithFirebase} >Log Out</button>
-          </li>
-        </ul>
-      );
-    }
-    else {
-      return (
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/faq' className="nav-link">FAQ</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/contact' className="nav-link">Contact Us</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/login' className="nav-link">Login</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/register' className="nav-link">Register</Link>
-          </li>
-        </ul>
-      );
-    }
-  }
-
-
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top float-right">
-        <div className="container-fluid">
-          <Link to='/' className="navbar-brand">GryphHacks</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {this.renderAuthenticatedLinks()}
-          </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top float-right">
+      <div className="container-fluid">
+        <Link to='/' className="navbar-brand">GryphHacks</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/faq' className="nav-link">FAQ</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/contact' className="nav-link">Contact Us</Link>
+            </li>
+            { !isAuthenticated &&
+            <li className="nav-item">
+              <Link to='/login' className="nav-link">Login</Link>
+            </li>
+            }
+            { !isAuthenticated &&
+            <li className="nav-item">
+              <Link to='/register' className="nav-link">Register</Link>
+            </li>
+            }
+            { isAuthenticated &&
+              <li className="nav-item">
+                <Link to='/dashboard' className="nav-link">Dashboard</Link>
+              </li>
+            }
+            { isAuthenticated &&
+              <li className="nav-item">
+                <button className='nav-link' onClick={() => logOutWithFirebase()} >Log Out</button>
+              </li>
+            }
+          </ul>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return { isAuthenticated: state.user.isAuthenticated }
-}
-
-
-export default connect(mapStateToProps, { logOut })(Navigation);
+export default Navigation;
