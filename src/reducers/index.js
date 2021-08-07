3cce5f3b7f30fd7f985a48e3c16a15d66ad91a16
“Muhammad <mmuttaki@uoguelph.ca>
2021-08-06 22:14:15 -0400

    Shop component added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import apiReducer from './apiReducer';
 import userReducer from './userReducer';
 
 export default combineReducers({
-  apiReducer: apiReducer,
   user: userReducer
 });
