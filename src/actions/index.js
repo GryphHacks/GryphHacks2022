535f8b02425a7e2b80e0a8c86f4053b9e98d1cda
“Muhammad <mmuttaki@uoguelph.ca>
2021-08-06 22:55:58 -0400

    Shop master initial code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,2 @@
-import { GET_SUMMARY } from "./types";
+import { FETCH_PRODUCTS } from "./types";
 import axios from 'axios';
-
-export const getSummary = () => {
-  return async dispatch => {
-    const response = await axios.get('https://api.covid19api.com/summary');
-    dispatch({ type: GET_SUMMARY, payload: response.data });
-  }
-}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,3 @@
 export const ADD_USER = 'ADD_USER';
 export const LOG_OUT = 'LOG_OUT';
+export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
