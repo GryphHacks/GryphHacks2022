d2c56bb05987d1c836aa5575bcb25caa0fd2615d
Prabhleen Ratra <prabhleenkaur2012@gmail.com>
2021-08-08 21:31:44 -0400

    Added Social Icons with links

--- src/components/pageComponents/Contact.js
+++ src/components/pageComponents/Contact.js
@@ -1,9 +1,24 @@
+import { 
+    FaDiscord, 
+    FaInstagram, 
+    FaTwitter, 
+    FaLinkedin} from 'react-icons/fa';
+
 const Contact = () => {
     return (
         <div>
             <h1>Contact Us!</h1>
+            <a href="https://discord.gg/JkBSZWY3" style={linkStyle}><FaDiscord size = {28}/></a>
+            <a href = "https://www.instagram.com/gryphhacks/" style={linkStyle}><FaInstagram size = {28} textDecoration={'none'}/></a>
+            <a href = "https://twitter.com/GryphHacks" style={linkStyle}><FaTwitter size = {28} textDecoration={'none'}/></a>
+            <a href = "https://www.linkedin.com/company/gryphhacks/mycompany/" style={linkStyle}><FaLinkedin size = {28} textDecoration={'none'}/></a>
         </div>
     )
 }
+
+const linkStyle = {
+    margin: "10px",
+    color: "inherit"
+  };
 
 export default Contact;
