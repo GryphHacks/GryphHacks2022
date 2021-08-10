277b9ab3ff578dcd768a15404ce4029c4f89c055
Eason <eason@Easons-MacBook-Pro.local>
2021-08-09 22:33:51 -0400

    Create Question Component
    Add a question component and temporary display

--- src/components/pageComponents/FAQ.js
+++ src/components/pageComponents/FAQ.js
@@ -1,7 +1,28 @@
+import Question from "./Question";
 const FAQ = () => {
+    const faq = [
+        {
+            question:"What is GryphHacks",
+            answer:"Hackathon"
+        },
+        {
+            question:"Where is GryphHacks Located",
+            answer:"Guelph,Ontario"
+        },
+        {
+            question:"Where do I sign up for GryphHacks",
+            answer:"Website"
+        },
+
+    ]
     return (
         <div>
             <h1>Frequently Asked Questions!</h1>
+            <br></br>
+    
+            {faq.map( ({question,answer}) => (
+          <Question question={question} answer={answer} />
+        ))}
         </div>
     )
 }
---
+++ src/components/pageComponents/Question.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import { IconName } from "react-icons/fa";
+import {FaChevronDown,FaChevronUp} from "react-icons/fa"
+const Question = ({question,answer}) => {
+    const [isClicked,setClicked] = useState(false)
+    return (
+        <div>
+                <div onClick = {()=>setClicked(!isClicked)}>
+                    <h6> {isClicked ? <FaChevronUp/>:<FaChevronDown/>} {question} </h6>
+                </div>
+                {isClicked && <div>{answer}</div>}
+         
+                <br></br>
+        </div>
+    )
+}
+
+export default Question
