1ed0a570d50d67df15bcacc47414f5af7731d4fd
Rabia Qureshi <rabia.q123@gmail.com>
2021-08-11 11:04:31 -0400

    Fixed error checking for empty input fields on Contact form

--- src/components/pageComponents/Contact.js
+++ src/components/pageComponents/Contact.js
@@ -1,25 +1,63 @@
+import { first, last } from "lodash";
+import { useState } from "react";
 import SocialBar from "../GeneralComponents.js/SocialBar"
 
 const Contact = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [hasError, setHasError] = useState(false);
+  const [error, setError] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e, firstName, lastName, email, message) => {
     e.preventDefault();
-    alert("Message sent successfully!");
+    handleErrors(firstName, lastName, email, message);
+    if (hasError) return;
+    console.log("Message sent successfully!");
   }
+
+  const handleErrors = (firstName, lastName, email, message) => {
+    if (firstName.length === 0) {
+      setHasError(true);
+      setError("Error: Enter your first name.");
+    } else if (lastName.length === 0) {
+      setHasError(true);
+      setError("Error: Enter your last name.");
+    } else if (email.length === 0) {
+      setHasError(true);
+      setError("Error: Enter your email.");
+    } else if (message.length === 0) {
+      setHasError(true);
+      setError("Error: Enter a message.");
+    } else {
+      setHasError(false);
+    }
+  } 
 
   return (
     <div>
       <h1>Contact Us!</h1>
       <br />
-      <form id="contact-form" onSubmit={(e) => handleSubmit(e)}>
+      <form id="contact-form" onSubmit={(e) => handleSubmit(e, firstName, lastName, email, message)} >
         <div className="form-group" id="name-form">
           <div id="first-name-form">
             <label htmlFor="name">First Name</label>
-            <input required type="text" className="form-control" />
+            <input 
+              required 
+              type="text" 
+              className="form-control" 
+              onChange={e => setFirstName(e.target.value)}
+            />
           </div>
           <div id="last-name-form">
             <label htmlFor="name">Last Name</label>
-            <input required type="text" className="form-control" />
+            <input 
+              required 
+              type="text" 
+              className="form-control"
+              onChange={e => setLastName(e.target.value)} 
+            />
           </div>
         </div>
         <div className="form-group">
@@ -29,16 +67,29 @@ const Contact = () => {
             type="email"
             className="form-control"
             aria-describedby="emailHelp"
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          <textarea required className="form-control" rows="5"></textarea>
+          <textarea 
+            required 
+            className="form-control" 
+            rows="5"
+            onChange={e => setMessage(e.target.value)}  
+          />
         </div>
         <br />
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <br />
+        {hasError && (
+          <div>
+            <br />
+            <p style={{ color: "red" }}>{error}</p>
+          </div>
+        )}
       </form>
       <br />
       <br />
