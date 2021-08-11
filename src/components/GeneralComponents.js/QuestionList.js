22d072fae7fc67fc3d9296018f5397af28afe877
Rabia Qureshi <rabia.q123@gmail.com>
2021-08-11 07:40:23 -0400

    Fixed inconsistent formatting between files

--- src/components/GeneralComponents.js/QuestionList.js
+++ src/components/GeneralComponents.js/QuestionList.js
@@ -1,21 +1,20 @@
-import Question from "./Question"
-const QuestionList = ({data}) => {
-    return (
-        <div>
-             <div style = {columns}>
-            {data.map( ({question,answer}) => (
-                        <Question question={question} answer={answer}/>
-                    ))}
-            </div> 
-          
-        </div>
-    )
-}
+import Question from "./Question";
 
-const columns ={
-    display: "grid",
-    gridTemplateColumns: "auto auto",
-  }
-  
-export default QuestionList
+const QuestionList = ({ data }) => {
+  return (
+    <div>
+      <div style={columns}>
+        {data.map(({ question, answer }) => (
+          <Question question={question} answer={answer} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
+const columns = {
+  display: "grid",
+  gridTemplateColumns: "auto auto",
+};
+
+export default QuestionList;
