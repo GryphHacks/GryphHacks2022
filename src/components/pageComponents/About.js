9392e6d4990cb4c74d9401b869b3a1d3467a9a7c
Andrew Linington <57372547+andrewlinington@users.noreply.github.com>
2021-08-12 02:12:14 -0400

    Added the data file for about us.

--- src/components/pageComponents/About.js
+++ src/components/pageComponents/About.js
@@ -1,67 +1,9 @@
 import TeamMemberList from "../GeneralComponents/TeamMemberList";
 import SocialBar from "../GeneralComponents/SocialBar"
 import TeamInfo from "../GeneralComponents/TeamInfo";
+import members from "../../data/team.json";
 
 const About = () => {
-    const members = [
-        {
-            name:"Andrew Linington",
-            title:"System Administrator",
-            image:"images/sampleImage.jpeg",
-            social: {
-                linkedin:"https://daveceddia.com/react-image-tag/"
-            }
-        },
-        {
-            name:"Andrew Linington",
-            title:"System Administrator",
-            image:"images/sampleImage.jpeg",
-            social: {
-                linkedin:"https://daveceddia.com/react-image-tag/"
-            }
-        },
-        {
-            name:"Andrew Linington",
-            title:"System Administrator",
-            image:"images/sampleImage.jpeg",
-            social: {
-                linkedin:"https://daveceddia.com/react-image-tag/"
-            }
-        },
-        {
-            name:"Andrew Linington",
-            title:"System Administrator",
-            image:"images/sampleImage.jpeg",
-            social: {
-                linkedin:"https://daveceddia.com/react-image-tag/"
-            }
-        },
-        {
-            name:"Andrew Linington",
-            title:"System Administrator",
-            image:"images/sampleImage.jpeg",
-            social: {
-                linkedin:"https://daveceddia.com/react-image-tag/"
-            }
-        },
-        {
-            name:"Andrew Linington",
-            title:"System Administrator",
-            image:"images/sampleImage.jpeg",
-            social: {
-                linkedin:"https://daveceddia.com/react-image-tag/"
-            }
-        },
-        {
-            name:"Andrew Linington",
-            title:"System Administrator",
-            image:"images/sampleImage.jpeg",
-            social: {
-                linkedin:"https://daveceddia.com/react-image-tag/"
-            }
-        },
-    ]
-
     return (
         <div>
             <TeamInfo/>
