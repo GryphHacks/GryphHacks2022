2f5ef740f5b9810ccc4b6d25868913dd7ef42e5b
Rabia Qureshi <rabia.q123@gmail.com>
2021-08-22 19:23:08 -0400

    Updated footer component styling

--- src/components/GeneralComponents/Footer.js
+++ src/components/GeneralComponents/Footer.js
@@ -3,9 +3,13 @@ import SocialBar from "./SocialBar"
 const Footer = () => {
   return (
     <div id="footer">
-      <SocialBar/>
+      <div className="in-footer">
+        <SocialBar />
+      </div>
+      <p className="in-footer">Newsletter component</p>
+      <p className="in-footer">Something else</p>
     </div>
-  )
+  );
 }
 
 export default Footer
