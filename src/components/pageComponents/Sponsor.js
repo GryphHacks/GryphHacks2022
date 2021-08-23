40dca77b2cf1442089abf44ee97b5b03fc64875d
Prabhleen Ratra <prabhleenkaur2012@gmail.com>
2021-08-22 21:54:05 -0400

    Added Pamphlet PDF Link

--- src/components/pageComponents/Sponsor.js
+++ src/components/pageComponents/Sponsor.js
@@ -1,7 +1,12 @@
+import Pdf from '../test.pdf';
+
 const Sponsor = () => {
     return (
         <div>
-            <p>Sponsor</p>
+            <h1>Sponsor</h1>
+            <br/>
+            <a href = {Pdf} target = "_blank">Pamphlet</a>
+            <p>Learn more here</p>
         </div>
     )
 }
