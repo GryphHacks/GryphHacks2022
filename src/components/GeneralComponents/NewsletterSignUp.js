7fb136d030781f7dcaea68167f76e2832bdb93cd
Brittany Mueller <brittany_mueller@sympatico.ca>
2021-08-24 22:32:52 -0400

    Added error message handling on newsletter submission.

--- src/components/GeneralComponents/NewsletterSignUp.js
+++ src/components/GeneralComponents/NewsletterSignUp.js
@@ -4,26 +4,61 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 const CustomForm = ({ status, message, onValidated }) => {
 
     const [email, setEmail] = useState('');
-    const [errorMsg, setErrorMsg] = useState('');
-    const [status, ]
+    const [isEmpty, setEmpty] = useState(null);     // Email field is empty by default
 
     const handleSubscribe = (event) => {
+
         event.preventDefault();
-        setErrorMsg('')
+        setEmpty(true)  // Reset state
 
-        console.log("before validated: ", status, message)
-        if (!email) {
-            setErrorMsg('Please enter a valid email address.')
-            return
+        if (email) {
+            // Email provided, attempt POST to mailchimp 
+            setEmpty(false)
+            onValidated({ MERGE0 : email });
         }
-        // email &&
-        // email.indexOf("@") > -1 &&
-        onValidated({ MERGE0 : email });
-        console.log("after validated: ", status, message)
-        
+    }
 
-        console.log(status, message)
-        setErrorMsg('')
+    const trimMessage = (message) => {
+
+        if (!message) {
+            // I don't think this will ever happen but just in case??
+            return null;
+        }
+        // Attempt to split the message at the dash
+        const result = message.split('-');
+
+        if ( "0" === result[0].trim() ) {
+            // The message was prefixed by a zero, return the trimmed message
+            return result[1]
+        }
+        // Otherwise no trimming to be done, return original msg
+        return message;
+    }
+
+    const handleMessage = (message) => {
+
+        if (isEmpty || message.includes('Please enter a value')) {
+            // MailChimp API does not error handle blank field, on spaces "0 - Please enter a value"
+            return 'Please enter a valid email address';
+
+        } else if (message.includes('domain portion')) {
+            // 0 - The domain portion of the email address is invalid (the portion after the @: .com)
+            return 'The domain of the provided email address is invalid';
+
+        } else if (message.includes('cannot be added') || message.includes('cannot be imported')) {
+            // This email cannot be added to this list. Please enter a different email address.
+            // admin@b.com is an invalid email address and cannot be imported.
+            return 'The email provided is invalid and cannot be added to this list. Please try a different email address';
+
+        } else {
+            // Use the provided MailChimp message, but trim any leading spaces/zeroes
+            // Too many subscribe attempts for this email address. Please try again in about 5 minutes.
+            // admin@bb.com is already subscribed to list GryphHacks. Click here to update your profile
+            // 0 - The username portion of the email address is empty
+            // 0 - An email address must contain a single @ 
+            // 0 - Enter a value less than 100 characters long
+            return trimMessage(message);
+        }
     }
 
     return (
@@ -41,12 +76,23 @@ const CustomForm = ({ status, message, onValidated }) => {
             
             {/* TODO - Add in MailChimp antispam fields at some point */}
 
-            {status === "sending" ? <p>Sending...</p> : null}
-            {status === "error" && <p style={{ color: "red" }}>{ errorMsg }</p>}
-            {status === "success" && <p style={{ color: "green" }}>Successfully subscribed! :)</p>}
+            {status === "sending" && <div style={{ color: "blue" }}>Sending...</div>}
+
+            {(status === "error" || isEmpty) && (
+                <div 
+                style={{ color: "red" }}
+                dangerouslySetInnerHTML={{ __html: handleMessage(message) }}
+                />
+            )}
+
+            {status === "success" && (
+                <div
+                style={{ color: "green" }}
+                dangerouslySetInnerHTML={{ __html: message }}
+                />
+            )}
         </>
     )
-
 }
 
 const NewsletterSignUp = () => {
