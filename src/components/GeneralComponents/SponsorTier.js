d866b8f0c75fac8ed654c140c7ece4ee4ac42dd3
Andrew Linington <57372547+andrewlinington@users.noreply.github.com>
2021-09-08 16:53:22 -0400

    added a basic sponsor tier component.

---
+++ src/components/GeneralComponents/SponsorTier.js
@@ -0,0 +1,21 @@
+const SponsorTier = ({tier, about, cost,  perks}) => {
+    return (
+        <div>
+            <div>
+                <h2>{tier}</h2>
+                <br/>
+                <h4>{about}</h4>
+            </div>
+           <div>
+               <h3>${cost}</h3>
+               <ul>
+                {perks.map(({item}) => (
+                    <li>{item}</li>
+                ))}
+               </ul>    
+           </div>
+        </div>
+    )
+}
+
+export default SponsorTier
