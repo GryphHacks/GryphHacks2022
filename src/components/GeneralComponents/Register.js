13016ce07b4511fa195f8526eacb072832cf5274
Eason <eliang@uoguelph.ca>
2021-10-15 02:41:50 -0400

    Create Register Component and SponsorLanding Component
    - Create a Register.js component for the landing page which will link user to the register section for the website
    -Applied CSS for Register.css based on Figma
    -Create a SponsorLanding.css and SponsorLanding.js Component for user to link to sponsors page and get more info as well as applied CSS to it
    -added components to landing page

---
+++ src/components/GeneralComponents/Register.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../GeneralComponents/GeneralComponentsCSS/Register.css"
+import { Link, useHistory } from "react-router-dom";
+const Register = () => {
+    const history = useHistory();
+    return (
+        <div>
+            <h6 id = "title">Register</h6>
+            <button id = "hackie" onClick = {()=>history.push("/register")}  >Hackie </button>
+            <button id = "mentor" onClick = {()=>history.push("/register")}>Mentor</button>
+           <br/>
+           <br/>
+  
+            <Link to ='/sponsor' className="link">Become a Sponsor </Link> 
+
+        </div>
+    )
+}
+
+export default Register
--- src/components/pageComponents/Landing.js
+++ src/components/pageComponents/Landing.js
@@ -4,11 +4,26 @@ import Judges from "../GeneralComponents/SpeakerAndJudgesComponents/Judges"
 import Speakers from "../GeneralComponents/SpeakerAndJudgesComponents/Speakers"
 import LogoShowcase from "../GeneralComponents/SponsorComponents/LogoShowcase"
 import SponsorCarousel from "../GeneralComponents/SponsorComponents/SponsorCarousel";
+import Register from "../GeneralComponents/Register";
+import SponsorLanding from "../GeneralComponents/SponsorLanding";
+import "./Landing.css"
 const Landing = () => {
   return (
     <div style={{textAlign:'center'}}>
-      <br/>
+      
       <Countdown/>
+      <Register/>
+
+    
+
+  
+      
+        <SponsorLanding title = "Sponsor Us" info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+
+      
+      <br/>
+
+
       <br/><br/>
       <NewsletterSignUp/>
       <br/>
