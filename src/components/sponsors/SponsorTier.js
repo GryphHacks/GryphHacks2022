983f690cbbc5542f001802c6b8f010f4dcb97a5c
Andrew Linington <57372547+andrewlinington@users.noreply.github.com>
2022-02-06 19:52:15 -0500

    Sponsor page content and screen
    added colors for each corisponding sponsor tier
    
    temp commit
    
    temp changes 2
    
    built out a basic design for 3 tiers of sponsors
    
    added colors for each corisponding sponsor tier
    
    temp commit

--- src/components/sponsors/SponsorTier.js
+++ src/components/sponsors/SponsorTier.js
@@ -1,18 +1,19 @@
-const SponsorTier = ({props: {tier, about, cost,  perks}}) => {
+const SponsorTier = ({props: {tier, about, cost, color,  perks}}) => {
     var id = 0;
 
     return (
-        <div>
-            <div>
+        <div className="sponsor-card shadow-below" style={{backgroundColor: color}}>
+            <div className="sponsor-about shadow-below">
                 <h2>{tier}</h2>
+                <hr className = "line-break" style={{background: color}}/>
                 <br/>
-                <h4>{about}</h4>
+                <h5 className="text-centered">{about}</h5>
             </div>
-           <div>
+           <div className="sponsor-info">
                <h3>${cost}</h3>
                <ul>
                 {perks.map(({item}) => (
-                    <li key= {id++}>{item}</li>
+                    <li key= {id++} className="info-list">{item}</li>
                 ))}
                </ul>    
            </div>
