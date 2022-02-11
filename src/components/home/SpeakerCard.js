d8f51978ea23d5f2d1d9d1c9293e0edc09b36444
Rabia Qureshi <rabia.q123@gmail.com>
2022-02-10 22:46:17 -0500

    Veritcally centered speaker image in Keynote Speakers section

--- src/components/home/SpeakerCard.js
+++ src/components/home/SpeakerCard.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-// have image on left
-// have name, description, main info on right 
-// all in one component
 const SpeakerCard = ({member}) => {
   return (
     <div style={speakerCardStyle}>
       <img style={speakerImageStyle} alt={member.name} src={member.image} />
       <div style={speakerInfoStyle}>
         <h4>{member.name}</h4>
-        <h5 style={speakerJobStyle}>{member.role}</h5>
+        <h5 style={{fontSize:"15px"}}>{member.role}</h5>
         <br/>
         <p>{member.description}</p>
       </div>
@@ -17,24 +14,16 @@ const SpeakerCard = ({member}) => {
   );
 }
 
-
 const speakerCardStyle = {
   display: "flex",
-  flexDirection: "row",
-  alignContent: "center", //idk why this isn't working
   columnGap: "5%",
-  width: "75%"
+  alignItems: "center"
 }
 
 const speakerImageStyle = {
   borderRadius: "50%",
   height:"200px",
   width:"200px",
-
-}
-
-const speakerJobStyle = {
-  fontSize: "15px"
 }
 
 const speakerInfoStyle = {
