de1aa885b1670d24763f05a690113577adeeda32
Rabia Qureshi <rabia.q123@gmail.com>
2022-02-13 00:28:52 -0500

    Added circuit image to sides of Keynote Speaker section

--- src/components/home/Speakers.js
+++ src/components/home/Speakers.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import speakers from "../../data/speaker.json";
 import SpeakerCard from './SpeakerCard';
-// import FooterCircuit from '../../assets/FooterCircuit.svg'
+import circuit from '../../assets/circuit.png'
 
 const Speakers = () => {
   return (
-    <div>
+    <div style={{"position": "relative"}}>
+      <img style={{...circuitImageStyle, ...{"left": "0", "top": "5%"}}} src={circuit} alt="circuit" />;
       <hr style={speakerSeparatorStyle} />
       <div style={speakerContent}>
-        <h1 style={{"letter-spacing": "10px"}}>KEYNOTE SPEAKERS</h1>
+        <h1 style={{ "letter-spacing": "10px" }}>KEYNOTE SPEAKERS</h1>
         <div style={speakerStyle}>
           <SpeakerCard member={speakers[0]} />
         </div>
       </div>
+      <img style={{...circuitImageStyle, ...{"transform": "rotate(180deg)", "right": "0", "bottom": "-15%"}}} src={circuit} alt="circuit" />
     </div>
   );
 }
 
+//circuit image
+const circuitImageStyle = {
+  position: "absolute",
+  height: "25%",
+  width: "25%",
+  zIndex: "-1",
+};
+
+// top separator
 const speakerSeparatorStyle = {
   backgroundColor: "var(--gryphyellow)",
   opacity: "1",
@@ -24,6 +35,7 @@ const speakerSeparatorStyle = {
   width: "100%",
 };
 
+// all text and content within the Keynote Speakers section on Landing
 const speakerContent = {
   display: "flex",
   flexDirection: "column",
@@ -32,12 +44,12 @@ const speakerContent = {
   paddingBottom: "5%",
 };
 
+// div around SpeakerCard component
 const speakerStyle = {
   marginTop: "3%",
-  padding: "5% 10%",
+  padding: "5% 5%",
   border: "2px solid rgba(221,17,51,.73)",
-  align: "center",
-  width: "80%"
+  width: "70%"
 };
 
 export default Speakers;
