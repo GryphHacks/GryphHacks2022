b262a902b1dfc31e3ed7a3452cb4d2077a8400cd
Madhurbain Kaur <kaurmadhurbain@gmail.com>
2022-02-23 16:30:38 -0500

    rough 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import './stylesheet/notFound.css';
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      Error 404 Not Found
+    <div className="errorPage">
+      <div className="errorContainer">
+        <h1 className="errorTitle"><span className="blink4">4</span>04</h1>
+        <h2 className="subText">Page not found.</h2>
+        <Link to="/"><button id="hackie">Home</button></Link>
+      </div>
     </div>
   );
 };
