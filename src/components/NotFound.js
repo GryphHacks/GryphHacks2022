9f1d1eb891b4e7ee63723cf148533b2eb8a57458
Eason <eliang@uoguelph.ca>
2022-02-24 00:19:39 -0500

    Merge branch 'master' into Eason

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import './stylesheet/notFound.css';
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      Error 404 Not Found
+    <div className="errorPage">
+      <div className="errorContainer">
+        <h1 className="errorTitle blink">404</h1>
+        <h2 className="subText">Page not found.</h2>
+        <Link to="/">
+          <button className='return-home'>Return to Home</button>
+        </Link>
+      </div>
     </div>
   );
 };
