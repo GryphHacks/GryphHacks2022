a85e6506d04fbb787e00a74602053b4ee115a3d2
Brittany Mueller <brittany_mueller@sympatico.ca>
2022-03-01 22:11:21 -0500

    Populated about us and finalized styling.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,22 +1,27 @@
 import TeamMemberList from "./TeamMemberList";
-import AboutInfo from "./AboutInfo";
 import members from "../../data/team.json";
 import './stylesheet/about.css'
 
 const About = () => {
     return (
         
-        <div>
-            <div id= "mainContainer">
-                <div id = "container">
-                    <AboutInfo/>
-                    <br/>
-                    <h1 id = "title">MEET OUR TEAM</h1>
-                    <br/>
-                    <TeamMemberList data = {members}/>
-                    <br/>
-                </div>
+        <div className="page-content">
+            <h2 className="section-title">WHO WE ARE</h2>
+            <div id= "about">
+                <p>
+                    Gryphhacks is the University of Guelph's first-ever, official hackathon! 
+                    GryphHacks consists of a team full of diverse students who are passionate about innovation and technology. 
+                    Our yearly goal is to organize a weekend full of collaboration, entrepreneurship and innovation at its finest, 
+                    while generating potential high-value product concepts and prototypes over a 48-hour period.
+                </p>
+                <p>
+                    Although we were super excited to welcome everyone to the University of Guelph to meet and collaborate with everyone in-person,
+                    the health and safety of our hackies, mentors, and judges remain our top priority. As a result, 
+                    we are hosting a fully virtual hackathon this year, open for everyone to join!
+                </p>
             </div>
+            <h2 className="section-title">MEET OUR TEAM</h2>
+            <TeamMemberList data = {members}/>
         </div>
     )
 }
--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -29,11 +29,11 @@ const Landing = () => {
       {/* Insert Mission component*/}
       <Mission/>
       <br/><br/><br/>
-      <SponsorLanding title = "Sponsor Us" info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+      <SponsorLanding title = "SPONSOR US" info = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
       <br/><br/><br/>
-      <SponsorCarousel/>
+      {/* <SponsorCarousel/> */}
       <br/><br/><br/>
-      <LogoShowcase/>
+      {/* <LogoShowcase/> */}
       <br/>  
       <Speakers/>
       <br/>
