d93325c6d645f9318721ce9783a08ec96acb39c9
Brittany Mueller <brittany_mueller@sympatico.ca>
2022-03-08 01:54:46 -0500

    Added coming soon data to speakers and judges sections.

--- src/components/home/GuestCard.js
+++ src/components/home/GuestCard.js
@@ -6,7 +6,7 @@ const GuestCard = ({ member }) => {
         <div>
             <div className="card-body">
                 <img className="card-img" src={member.image} alt={member.name + "'s Profile image"}></img>
-                <h5 className="card-title">{member.name}</h5>
+                <h4 className="card-title">{member.name}</h4>
                 <p className="card-text">{member.role}</p>
             </div>
 
