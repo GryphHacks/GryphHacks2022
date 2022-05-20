390c2c13812451edae73aee09a55149f32868e25
Eason <eliang@uoguelph.ca>
2022-05-20 11:55:00 -0400

    Set carousel cards to be fixed sizes and set all text to vertically align

--- src/components/home/SpeakerCard.js
+++ src/components/home/SpeakerCard.js
@@ -4,8 +4,9 @@ import "./stylesheet/speakers.css"
 const SpeakerCard = ({ member }) => {
   return (
     <div className="cardStyle">
-      <img style={photoStyle} alt={member.name} src={member.image} />
-      <div style={infoCardStyle}>
+      <img className="photoStyle" alt={member.name} src={member.image} />
+      <div className="infoCardStyle">
+        <div className="textStyle">
         <h4>{member.name}</h4>
         <h5 style={{fontSize: "15px"}}>{member.role}</h5>
         <br/>
@@ -16,6 +17,8 @@ const SpeakerCard = ({ member }) => {
           {member.social.github && <a href = {member.social.github}  target='_blank' rel='noreferrer'><FaGithub size = {25} textDecoration={'none'}/></a>}
           {member.social.twitter && <a href = {member.social.twitter}  target='_blank' rel='noreferrer'><FaTwitter size = {25} textDecoration={'none'}/></a>}
         </div>
+        </div>
+
       </div>
     </div>
   );
@@ -23,37 +26,45 @@ const SpeakerCard = ({ member }) => {
 
 // main Speakers component content - Speaker Card (photo and info section)
 // image in Speaker Card
-const photoStyle = {
-  borderRadius: "50%",
-  border: "7px solid rgba(221,17,51,.73)",
-  height: "225px",
-  width: "225px",
-  padding: "1px"
-};
+// const photoStyle = {
+//   borderRadius: "50%",
+//   border: "7px solid rgba(221,17,51,.73)",
+//   height: "225px",
+//   width: "225px",
+//   padding: "1px"
+// };
 
-// info section in Speaker Card
-const infoCardStyle = {
-  textAlign: "left",
-  position: "relative",
-  backgroundColor: "#8A8A8A",
-  color: "white",
-  padding: "4%",
-  width: "60%",
-  borderRadius: "10px",
-  boxShadow: "-3px 3px 3px rgb(122,122,122)",
-};
+// // info section in Speaker Card
+// const infoCardStyle = {
+  
+//   textAlign: "left",
+//   verticleAlign:"middle!important",
+//   position: "relative",
+//   backgroundColor: "#8A8A8A",
+//   color: "white",
+//   padding: "4%",
+//   width: "60%",
+//   borderRadius: "10px",
+//   height:"700px",
+//   boxShadow: "-3px 3px 3px rgb(122,122,122)",
+//   // border: "solid 2px green",
+// };
 
+// const textStyle = {
+//   border:"solid 2px green"
+// }
 // bottom layer of info section of Speaker Card
-const layerStyle = {
-  position: "absolute",
-  zIndex: "-1", // z-index applies only to absolutely positioned elements
-  height: "100%",
-  width: "100%",
-  top: "8px",
-  right: "8px",
-  backgroundColor: "rgba(221,17,51,.73)",
-  borderRadius: "10px",
-  boxShadow: "-5px 5px 10px rgb(180, 186, 194)",
-};
+// const layerStyle = {
+//   position: "absolute",
+//   zIndex: "-1", // z-index applies only to absolutely positioned elements
+//   height: "100%",
+//   width: "100%",
+//   top: "8px",
+//   right: "8px",
+//   backgroundColor: "rgba(221,17,51,.73)",
+//   borderRadius: "10px",
+//   boxShadow: "-5px 5px 10px rgb(180, 186, 194)",
+//   border:"solid 2px blue"
+// };
 
 export default SpeakerCard;
